e3d8a1700a1abc1a470f4ead8c97adcef846ef70
hahamen-ihs <52111301+hahamen-ihs@users.noreply.github.com>
2019-09-24 22:26:26 +0700

    Add files via upload

---
+++ robot_vision_raspi.py
@@ -0,0 +1,75 @@
+import cv2.cv as cv     # import libarary openCV
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+capture = cv.CaptureFromCAM(0)
+cv.SetCaptureProperty(capture,3,640)
+cv.SetCaptureProperty(capture,4,480)
+time.sleep(2)
+
+while True:
+    img = cv.QueryFrame(capture)
+    cv.Smooth(img,img,cv.CV_BLUR,3)
+    hue_img = cv.CreateImage(cv.GetSize(img),8,3)
+    cv.CvtColor(img,hue_img,cv.CV_BGR2HSV)
+    threshold_img=cv.CreateImage(cv.GetSize(hue_img),8,1)
+    cv.InRangeS(hue_img,(0,150,0),(5,255,255),threshold_img)
+    storage = cv.CreateMemStorage(0)
+    contour = cv.FindContours(threshold_img,storage,cv.CV_RETR_CCOMP,cv.CV_CHAIN_APPROX_SIMPLE)
+    points = []
+    while contour:
+        rect = cv.BoundingRect(list(contour))
+
+        # konversi koordinat objek menjadi logika GPIO
+        if rect[0]>426.67 and rect[0]<640.0 and rect[1]>0.0 and rect[1]<240.0:
+            print ('BELOK KANAN SEDANG')
+            GPIO.output(14, False)
+            GPIO.output(15, True)
+            GPIO.output(23, False)
+            GPIO.output(24, False)
+        if rect[0]>426.67 and rect[0]<640.0 and rect[1]>240.0 and rect[1]<480.0:
+            print ('BELOK KANAN CURAM')
+            GPIO.output(14, False)
+            GPIO.output(15, True)
+            GPIO.output(23, True)
+            GPIO.output(24, False)
+        if rect[0]>213.33 and rect[0]<426.67 and rect[1]>0.0 and rect[1]<240.0:
+            print ('MAJU KEDEPAN')
+            GPIO.output(14, False)
+            GPIO.output(15, True)
+            GPIO.output(23, False)
+            GPIO.output(24, True)
+        if rect[0]>213.33 and rect[0]<426.67 and rect[1]>240.0 and rect[1]<480.0:
+            print ('DIAM')
+            GPIO.output(14, False)
+            GPIO.output(15, False)
+            GPIO.output(23, False)
+            GPIO.output(24, False)
+        if rect[0]>0.0 and rect[0]<213.33 and rect[1]>0.0 and rect[1]<240.0:
+            print ('BELOK KIRI SEDANG')
+            GPIO.output(14, False)
+            GPIO.output(15, False)
+            GPIO.output(23, False)
+            GPIO.output(24, True)
+        if rect[0]>0.0 and rect[0]<213.33 and rect[1]>240.0 and rect[1]<480.0:
+            print ('BELOK KIRI CURAM')
+            GPIO.output(14, True)
+            GPIO.output(15, False)
+            GPIO.output(23, False)
+            GPIO.output(24, True)
+        
+        contour = contour.h_next()
+        size = (rect[2]*rect[3])
+        if size > 100:
+            pt1 = (rect[0],rect[1])
+            pt2 = (rect[0]+rect[2],rect[1]+rect[3])
+            cv.Rectangle(img,pt1,pt2,(38,160,60))
+
+    cv.ShowImage("Colour Tracking",img)
+    if cv.WaitKey(10)==27:
+        break
